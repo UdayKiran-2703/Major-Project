6fabd8b16c81b589c88123b9094beb552aafe113
Uday Kiran <modhuguudaykiran123@gmail.com>
2024-02-03 18:00:42 +0530

    third commit

--- models/listing.js
+++ models/listing.js
@@ -4,7 +4,7 @@ const listingSchema= new Schema({
     title:{type:String,required:true},
     description:String,
     image:{type:String,
-        default:"https://unsplash.com/photos/a-lone-sailboat-in-the-middle-of-the-ocean-uI3CBm8udb4",
+        default:"https://images.unsplash.com/photo-1635864060824-87c462dd4a5a?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     set:(v)=>v===""?"https://unsplash.com/photos/a-lone-sailboat-in-the-middle-of-the-ocean-uI3CBm8udb4":v
     },
     price:Number,
